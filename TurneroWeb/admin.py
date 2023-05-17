c60a13a6038516142e0ea0b371b846817f0fe6b5
Seba Olima <edificarvcp@gmail.com>
2023-05-17 12:54:35 -0300

    probando conexion db con el servidor

---
+++ TurneroWeb/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Especialista
+from .models import turnosPorEspecialista
+
+class turnosPorEspecialistaAdmin(admin.ModelAdmin):
+    list_display = ("fecha","horarioDeInicio","horarioDeFin","horarioDeTurno")
+class EspecialistaAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "especialidad","horariosDeAtencion","id_Turnos")
+
+
+admin.site.register(Especialista, EspecialistaAdmin)
+admin.site.register(turnosPorEspecialista, turnosPorEspecialistaAdmin)
---
+++ TurneroWeb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TurnerowebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'TurneroWeb'
